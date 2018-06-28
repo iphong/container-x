272ed76b4596d06377c9ce81fc337cf4cad2ae01
Phong Vu <phong.vu@me.com>
2018-06-28 07:30:15 +0700

    Update

---
+++ src/container-x.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = require("react");
+class Container {
+    constructor(state) {
+        this.listeners = [];
+        this.state = state;
+    }
+    setState(nextState, callback) {
+        this.state = Object.assign({}, this.state, nextState);
+        return Promise.all(this.listeners.map(fn => fn(nextState)));
+    }
+    subscribe(fn) {
+        this.listeners.push(fn);
+    }
+    unsubscribe(fn) {
+        this.listeners = this.listeners.filter(f => f !== fn);
+    }
+}
+exports.Container = Container;
+class Subscribe extends react_1.Component {
+    constructor() {
+        super(...arguments);
+        this.onUpdate = (changes) => {
+            return new Promise(resolve => {
+                Object.keys(changes).some(k => this.props.bind.includes(k)) ? this.setState({}, resolve) : resolve();
+            });
+        };
+    }
+    componentDidMount() {
+        this.props.to.subscribe(this.onUpdate);
+    }
+    componentWillUnmount() {
+        this.props.to.unsubscribe(this.onUpdate);
+    }
+    render() {
+        return this.props.children(...this.props.bind.map(p => this.props.to.state[p]));
+    }
+}
+exports.Subscribe = Subscribe;
+//# sourceMappingURL=container-x.js.map
